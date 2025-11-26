ef1c9eb4a87e700e86f24969fe3c0b76d6db6422
Youjin-n <m.kucukcoskunn@gmail.com>
2025-11-26 08:29:49 +0300

    Add log_entry.py module

---
+++ src/log_entry.py
@@ -0,0 +1,7 @@
+class LogEntry:
+    def __init__(self, ip, timestamp, method, endpoint, status_code):
+        self.ip = ip
+        self.timestamp = timestamp
+        self.method = method
+        self.endpoint = endpoint
+        self.status_code = status_code
